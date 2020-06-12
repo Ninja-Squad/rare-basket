e6d30a17652524f8251bd0e9630eb57c7786351e
Jean-Baptiste Nizet <jb@ninja-squad.com>
2020-06-12 10:26:39 +0200

    chore: upgrade to latest spring boot

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,6 @@
 pluginManagement {
     plugins {
-        id("org.springframework.boot") version "2.3.0.RELEASE"
+        id("org.springframework.boot") version "2.3.1.RELEASE"
         id("io.spring.dependency-management") version "1.0.9.RELEASE"
         id("org.flywaydb.flyway") version "6.4.4"
         id("com.github.node-gradle.node") version "2.2.4"
