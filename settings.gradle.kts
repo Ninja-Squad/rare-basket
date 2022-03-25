6c6fd69295a5d844eaa84176237eecb5ec478c1b
jnizet <jb@ninja-squad.com>
2022-03-25 10:33:18 +0100

    chore: upgrade to Spring Boot 2.6.5

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,6 @@
 pluginManagement {
     plugins {
-        id("org.springframework.boot") version "2.6.4"
+        id("org.springframework.boot") version "2.6.5"
         id("io.spring.dependency-management") version "1.0.11.RELEASE"
         id("org.flywaydb.flyway") version "8.0.5"
         id("com.github.node-gradle.node") version "3.2.1"
