37fbe063d6f57990fac14bd2e4707a89eb0be2c1
jnizet <jb@ninja-squad.com>
2022-09-23 08:39:54 +0200

    chore: upgrade flyway gradle plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,7 +2,7 @@ pluginManagement {
     plugins {
         id("org.springframework.boot") version "2.7.4"
         id("io.spring.dependency-management") version "1.0.14.RELEASE"
-        id("org.flywaydb.flyway") version "8.5.9"
+        id("org.flywaydb.flyway") version "8.5.13"
         id("com.github.node-gradle.node") version "3.4.0"
     }
 }
