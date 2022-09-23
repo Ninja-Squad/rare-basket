d0f43f098b99868d959a66e1399c111f2c38194f
jnizet <jb@ninja-squad.com>
2022-09-23 08:26:25 +0200

    chore: upgrade to spring boot 2.7.4

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,7 @@
 pluginManagement {
     plugins {
-        id("org.springframework.boot") version "2.6.7"
-        id("io.spring.dependency-management") version "1.0.11.RELEASE"
+        id("org.springframework.boot") version "2.7.4"
+        id("io.spring.dependency-management") version "1.0.14.RELEASE"
         id("org.flywaydb.flyway") version "8.5.9"
         id("com.github.node-gradle.node") version "3.2.1"
     }
