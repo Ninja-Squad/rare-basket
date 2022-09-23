0141fe9a0421ab244a75f3130f5d6111638e4d60
jnizet <jb@ninja-squad.com>
2022-09-23 08:43:04 +0200

    chore: upgrade itext version

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -80,7 +80,7 @@ dependencyManagement {
 }
 
 dependencies {
-    val itextVersion = "7.2.2"
+    val itextVersion = "7.2.3"
 
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
