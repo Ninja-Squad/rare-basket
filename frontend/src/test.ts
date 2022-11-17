5a0726a2691cc4fdf6f76055008eab1723ceef09
jnizet <jb@ninja-squad.com>
2022-11-17 10:42:16 +0100

    chore: upgrade to Angular 15

--- frontend/src/test.ts
+++ frontend/src/test.ts
@@ -5,23 +5,8 @@ import { getTestBed } from '@angular/core/testing';
 import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
 import { speculoosMatchers } from 'ngx-speculoos';
 
-declare const require: {
-  context(
-    path: string,
-    deep?: boolean,
-    filter?: RegExp
-  ): {
-    keys(): string[];
-    <T>(id: string): T;
-  };
-};
-
 // First, initialize the Angular testing environment.
 getTestBed().initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting());
-// Then we find all the tests.
-const context = require.context('./', true, /\.spec\.ts$/);
-// And load the modules.
-context.keys().map(context);
 
 beforeEach(() => {
   jasmine.addMatchers(speculoosMatchers);
