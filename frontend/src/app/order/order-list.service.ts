dfc8d62016539affdbfeeb7b994c83d060264d81
jnizet <jb@ninja-squad.com>
2023-11-09 16:32:10 +0100

    doc: document the order list service
    also fix a typo

--- frontend/src/app/order/order-list.service.ts
+++ frontend/src/app/order/order-list.service.ts
@@ -14,6 +14,16 @@ export interface OrderListViewModel {
   orders: Page<Order>;
 }
 
+/**
+ * Service used by the two components listing orders (in progress and done), in order to avoid duplicating their code,
+ * which is almost identical.
+ * It allows creating an observable, which is supposed to be subscribed once and only once, which, when subscribed
+ * - listens to the activated route query params in order to load the appropriate page of orders and emit it
+ * - gets the currently authenticated user, necessary to know which accession holders they can access
+ * - populates a form control with the appropriate accession holder ID, retrieved (if present) from the query params
+ * - navigates to the current route with the page set to 0 and the selected accession holder ID in the query params
+ *   whenever the accession holder form control changes its value
+ */
 @Injectable({
   providedIn: 'root'
 })
@@ -24,10 +34,16 @@ export class OrderListService {
     private router: Router
   ) {}
 
+  /**
+   * Creates the observable for the "in progress" orders
+   */
   setupInProgress(route: ActivatedRoute, accessionHolderIdCtrl: FormControl<number | null>): Observable<OrderListViewModel> {
     return this.setup(route, accessionHolderIdCtrl, (page, accessionHolderId) => this.orderService.listInProgress(page, accessionHolderId));
   }
 
+  /**
+   * Creates the observable for the "done" orders
+   */
   setupDone(route: ActivatedRoute, accessionHolderIdCtrl: FormControl<number | null>): Observable<OrderListViewModel> {
     return this.setup(route, accessionHolderIdCtrl, (page, accessionHolderId) => this.orderService.listDone(page, accessionHolderId));
   }
@@ -39,7 +55,10 @@ export class OrderListService {
   ): Observable<OrderListViewModel> {
     const vm$: Observable<OrderListViewModel> = combineLatest([route.queryParamMap, this.authenticationService.getCurrentUser()])
       .pipe(
+        // when the query params change, set the value of the accession holder form control (only if the value is not already
+        // the correct one)
         tap(([params, user]) => this.populateAccessionHolder(accessionHolderIdCtrl, params, user)),
+        // when the query params change, load the page of orders and combine it with the current user
         switchMap(([params, user]) => {
           const accessionHolderIdAsString = params.get('h');
           const accessionHolderId = accessionHolderIdAsString ? parseInt(accessionHolderIdAsString) : null;
@@ -49,13 +68,15 @@ export class OrderListService {
       )
       .pipe();
 
+    // create an observable which emits exactly the same events as vm$, but which, when subscribed,
+    // also subscribes to the value changes of the form control, in order to navigate when it changes
     return merge(
       vm$,
       accessionHolderIdCtrl.valueChanges.pipe(
         tap(accessionHolderId => {
           this.router.navigate(['.'], { relativeTo: route, queryParams: { page: 0, h: accessionHolderId ?? undefined } });
         }),
-        ignoreElements()
+        ignoreElements() // to avoid actually merging any elements to the vm$ observable
       )
     );
   }
