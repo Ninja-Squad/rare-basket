638d8bf79a831ae5f7875de4cc9b009e8a6c7c5d
jnizet <jb@ninja-squad.com>
2024-05-31 06:55:07 +0200

    chore: upgrade to Spring Boot 3.3.0

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,6 @@
 pluginManagement {
     plugins {
-        id("org.springframework.boot") version "3.2.5"
+        id("org.springframework.boot") version "3.3.0"
         id("io.spring.dependency-management") version "1.1.5"
         id("org.flywaydb.flyway") version "10.13.0"
         id("com.github.node-gradle.node") version "7.0.2"
