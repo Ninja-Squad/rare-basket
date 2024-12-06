7f6e96fa44a0937deb072487c97e35172f280988
cexbrayat <cedric@ninja-squad.com>
2024-12-06 16:26:13 +0100

    refactor: use MockitoBean

--- backend/src/test/java/fr/inra/urgi/rarebasket/web/accessionholder/AccessionHolderControllerTest.java
+++ backend/src/test/java/fr/inra/urgi/rarebasket/web/accessionholder/AccessionHolderControllerTest.java
@@ -30,10 +30,10 @@ import org.mockito.ArgumentCaptor;
 import org.mockito.Captor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 /**
@@ -43,13 +43,13 @@ import org.springframework.test.web.servlet.MockMvc;
 @ActiveProfiles("test")
 @WithMockUser
 class AccessionHolderControllerTest {
-    @MockBean
+    @MockitoBean
     private CurrentUser mockCurrentUser;
 
-    @MockBean
+    @MockitoBean
     private AccessionHolderDao mockAccessionHolderDao;
 
-    @MockBean
+    @MockitoBean
     private GrcDao mockGrcDao;
 
     @Autowired
