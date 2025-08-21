ae3bab9678d558ce8bf52d5fe02b6fe68e844d0a
jnizet <jb@ninja-squad.com>
2025-08-21 16:43:17 +0200

    chore: use Angular 20.2.1 and zoneless CD

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,11 +1,11 @@
-import { provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
 import { AppComponent } from './app/app.component';
 import { provideI18n } from './app/i18n/i18n';
 import { APP_ROUTES } from './app/app.routes';
 import { provideRouter, withViewTransitions } from '@angular/router';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AuthenticationConfigService, authFactory, CustomSecurityStorage } from './app/shared/authentication-config.service';
-import { AbstractSecurityStorage, StsConfigLoader, provideAuth } from 'angular-auth-oidc-client';
+import { AbstractSecurityStorage, provideAuth, StsConfigLoader } from 'angular-auth-oidc-client';
 import { errorInterceptor } from './app/shared/error.interceptor';
 import { authenticationInterceptor } from './app/shared/authentication.interceptor';
 import { provideHttpClient, withInterceptors, withNoXsrfProtection } from '@angular/common/http';
@@ -13,7 +13,7 @@ import { provideNgbDatepickerServices } from './app/rb-ngb/datepicker-providers'
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideZonelessChangeDetection(),
     provideBrowserGlobalErrorListeners(),
     provideAuth({
       loader: {
