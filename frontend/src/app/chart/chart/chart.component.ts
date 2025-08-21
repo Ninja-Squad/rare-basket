eedb2900724c48492e6efda30b054e9f43cbc40b
jnizet <jb@ninja-squad.com>
2025-08-21 17:21:58 +0200

    refactor: remove now useless runOutsideAngular call

--- frontend/src/app/chart/chart/chart.component.ts
+++ frontend/src/app/chart/chart/chart.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, NgZone, inject, input, viewChild, afterRenderEffect, ChangeDetectionStrategy } from '@angular/core';
+import { afterRenderEffect, ChangeDetectionStrategy, Component, ElementRef, input, viewChild } from '@angular/core';
 import { Chart, ChartConfiguration } from 'chart.js';
 
 @Component({
@@ -8,17 +8,13 @@ import { Chart, ChartConfiguration } from 'chart.js';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ChartComponent {
-  private readonly zone = inject(NgZone);
-
   readonly configuration = input.required<ChartConfiguration<'doughnut', Array<number>>>();
   readonly canvas = viewChild.required<ElementRef<HTMLCanvasElement>>('canvas');
 
   constructor() {
     afterRenderEffect(onCleanup => {
-      this.zone.runOutsideAngular(() => {
-        const chart = new Chart(this.canvas().nativeElement, this.configuration());
-        onCleanup(() => chart.destroy());
-      });
+      const chart = new Chart(this.canvas().nativeElement, this.configuration());
+      onCleanup(() => chart.destroy());
     });
   }
 }
