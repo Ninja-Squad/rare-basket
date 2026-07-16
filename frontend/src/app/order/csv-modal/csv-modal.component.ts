25da99477f33bb2f6affeae931cd78db602d4d16
cexbrayat <cedric@ninja-squad.com>
2026-07-16 17:12:30 +0200

    refactor: migrate CSV modal form to signal forms

--- frontend/src/app/order/csv-modal/csv-modal.component.ts
+++ frontend/src/app/order/csv-modal/csv-modal.component.ts
@@ -1,38 +1,28 @@
-import { ChangeDetectionStrategy, Component, inject, Signal } from '@angular/core';
-import { NonNullableFormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { ChangeDetectionStrategy, Component, computed, inject, signal } from '@angular/core';
+import { form, FormField, FormRoot } from '@angular/forms/signals';
 import { CsvResult, OrderCsvParserService } from '../order-csv-parser.service';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { AccessionComponent } from '../../shared/accession/accession.component';
 import { DecimalPipe } from '@angular/common';
-import { FormControlValidationDirective } from '../../shared/form-control-validation.directive';
 import { TranslateDirective, TranslatePipe } from '@ngx-translate/core';
-import { toSignal } from '@angular/core/rxjs-interop';
-import { map } from 'rxjs';
 
 @Component({
   selector: 'rb-csv-modal',
   templateUrl: './csv-modal.component.html',
   styleUrl: './csv-modal.component.scss',
-  imports: [TranslateDirective, TranslatePipe, ReactiveFormsModule, FormControlValidationDirective, AccessionComponent, DecimalPipe],
+  imports: [TranslateDirective, TranslatePipe, FormRoot, FormField, AccessionComponent, DecimalPipe],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CsvModalComponent {
   private readonly modal = inject(NgbActiveModal);
+  private readonly csvParser = inject(OrderCsvParserService);
 
-  readonly form = inject(NonNullableFormBuilder).group({
-    csv: ''
+  readonly formValue = signal({ csv: '' });
+  readonly form = form(this.formValue);
+  readonly result = computed<CsvResult>(() => {
+    const csv = this.form.csv().value();
+    return csv ? this.csvParser.parse(csv) : { errors: [], items: [] };
   });
-  readonly result: Signal<CsvResult>;
-
-  constructor() {
-    const csvParser = inject(OrderCsvParserService);
-    this.result = toSignal(this.form.controls.csv.valueChanges.pipe(map(csv => csvParser.parse(csv))), {
-      initialValue: {
-        errors: [],
-        items: []
-      }
-    });
-  }
 
   close() {
     this.modal.close(this.result().items);
